6e81690812884c2ef434423aa4f76dd8b3b20e68
smplrtrn <smplrtrn@gmail.com>
2022-04-05 16:29:09 +0200

    init

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import cloudflare from '@sveltejs/adapter-cloudflare';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: cloudflare(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
+};
+
+export default config;
